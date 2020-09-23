62c7ee6a11dba89259fdecf363a1a97d11f48ed9
cahyajuliannn <cahyajuliannn@gmail.com>
2020-09-23 12:08:00 +0700

    add InitDB

--- ReimbursementParking/ReimbursementParkingAPI/Context/MyContext.cs
+++ ReimbursementParking/ReimbursementParkingAPI/Context/MyContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ReimbursementParkingAPI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +11,15 @@ namespace ReimbursementParkingAPI.Context
     {
         public MyContext(DbContextOptions<MyContext> options) : base(options)
         {
+        }
+        public DbSet<RequestReimbursementStatusEnum> RequestReimbursementStatusEnums { get; set; }
+        public DbSet<RequestReimbursementParking> RequestReimbursementParkings { get; set; }
+        public DbSet<Blob> Blobs { get; set; }
+        public DbSet<RequestDetail> RequestDetails { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
         }
     }
 }
---
+++ ReimbursementParking/ReimbursementParkingAPI/Models/RequestDetail.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReimbursementParkingAPI.Models
+{
+    [Table("tb_plat-number")]
+    public class RequestDetail
+    {
+        public int Id { get; set; }
+        public string PlatNumber { get; set; }
+        public string VechicleType { get; set; }
+        public string PaymentType { get; set; }
+        public int TotalPrice { get; set; }
+        public string VechicleOwner { get; set; }
+        public string ParkingName { get; set; }
+        public string ParkingAddress { get; set; }
+
+        public int RequestReimbursementParkingId { get; set; }
+        public RequestReimbursementParking RequestReimbursementParking { get; set; }
+    }
+}
