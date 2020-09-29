07a6672d9c2c4f30b1fbdf849642a9cb96809a05
cahyajuliannn <cahyajuliannn@gmail.com>
2020-09-29 21:04:45 +0700

    add ErrorHandlerFix

---
+++ ReimbursementParking/ReimbursementParkingAPI/ViewModels/ManagerDateRangeVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReimbursementParkingAPI.ViewModels
+{
+    public class ManagerDateRangeVM
+    {
+        public string Start { get; set; }
+        public string End { get; set; }
+    }
+}
